e4885d83e7e58120e66c1b58cac1209644868a9b
YacineFodil <medyacinefodil@gmail.com>
2021-08-19 10:34:07 +0200

    change message final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('test 1 2 1 2'));
+app.get('/', (req, res) => res.send('test finale'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
